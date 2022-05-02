3970fd6cef1eb8d1bbc3f5bab373639f1f45b479
PsychicPlatypus <dzenismadzovic@gmail.com>
2022-05-02 13:21:32 +0200

    Additional commits

--- .qt_for_python/uic/main_screen.py
+++ .qt_for_python/uic/main_screen.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(280, 389)
+        MainWindow.resize(266, 389)
         MainWindow.setStyleSheet("background-color: rgb(220, 221, 255);\n"
 "border-top-color: rgb(255, 255, 255);\n"
 "margin-left: 30px;\n"
@@ -23,77 +23,64 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
-        self.widget = QtWidgets.QWidget(self.centralwidget)
-        self.widget.setObjectName("widget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.RoundButton = QtWidgets.QPushButton(self.widget)
-        self.RoundButton.setStyleSheet("QPushButton{\n"
-"background-color:  rgb(255, 203, 99);\n"
+        self.RoundButton = QtWidgets.QPushButton(self.centralwidget)
+        self.RoundButton.setStyleSheet("background-color:  rgb(255, 203, 99);\n"
 "aspect-ratio: 1 / 1;\n"
 " color: white;\n"
 " border-width: 0px;\n"
 " border-radius:50px;\n"
 " min-width:100px;\n"
-" min-height:100px;\n"
-"}\n"
-"QPushButton:Hover{\n"
-"background-color: rgb(255, 220, 99);\n"
-"}")
+" min-height:100px;")
         self.RoundButton.setObjectName("RoundButton")
         self.horizontalLayout_2.addWidget(self.RoundButton)
-        self.widget1 = QtWidgets.QWidget(self.widget)
-        self.widget1.setMinimumSize(QtCore.QSize(100, 100))
-        self.widget1.setObjectName("widget1")
-        self.horizontalLayout_2.addWidget(self.widget1)
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setMinimumSize(QtCore.QSize(100, 100))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout_2.addWidget(self.widget)
         self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        self.widget_2 = QtWidgets.QWidget(self.widget)
+        self.widget_2 = QtWidgets.QWidget(self.centralwidget)
         self.widget_2.setMinimumSize(QtCore.QSize(100, 100))
         self.widget_2.setObjectName("widget_2")
         self.horizontalLayout.addWidget(self.widget_2)
-        self.pushButton_2 = QtWidgets.QPushButton(self.widget)
-        self.pushButton_2.setStyleSheet("QPushButton{\n"
-"background-color:  rgb(255, 203, 99);\n"
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setStyleSheet("background-color:  rgb(255, 203, 99);\n"
 "aspect-ratio: 1 / 1;\n"
 " color: white;\n"
 " border-width: 0px;\n"
 " border-radius:50px;\n"
+" max-width:100px;\n"
+" max-height:100px;\n"
 " min-width:100px;\n"
-" min-height:100px;\n"
-"}\n"
-"QPushButton:Hover{\n"
-"background-color: rgb(255, 220, 99);\n"
-"}")
+" min-height:100px;")
         self.pushButton_2.setObjectName("pushButton_2")
         self.horizontalLayout.addWidget(self.pushButton_2)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.pushButton_3 = QtWidgets.QPushButton(self.widget)
-        self.pushButton_3.setStyleSheet("QPushButton{\n"
-"background-color:  rgb(255, 203, 99);\n"
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setStyleSheet("background-color:  rgb(255, 203, 99);\n"
 "aspect-ratio: 1 / 1;\n"
 " color: white;\n"
 " border-width: 0px;\n"
 " border-radius:50px;\n"
+" max-width:100px;\n"
+" max-height:100px;\n"
 " min-width:100px;\n"
-" min-height:100px;\n"
-"}\n"
-"QPushButton:Hover{\n"
-"background-color: rgb(255, 220, 99);\n"
-"}")
+" min-height:100px;")
         self.pushButton_3.setObjectName("pushButton_3")
         self.horizontalLayout_3.addWidget(self.pushButton_3)
-        self.widget_3 = QtWidgets.QWidget(self.widget)
+        self.widget_3 = QtWidgets.QWidget(self.centralwidget)
         self.widget_3.setMinimumSize(QtCore.QSize(100, 100))
         self.widget_3.setObjectName("widget_3")
         self.horizontalLayout_3.addWidget(self.widget_3)
         self.verticalLayout.addLayout(self.horizontalLayout_3)
-        self.gridLayout.addWidget(self.widget, 0, 0, 1, 1, QtCore.Qt.AlignHCenter|QtCore.Qt.AlignVCenter)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
