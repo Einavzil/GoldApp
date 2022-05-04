829ded9865f2a608d88929683b4cb866cadc2980
Liis U <liis.usin@gmail.com>
2022-05-04 20:35:32 +0200

    trying to enter data

---
+++ src/LiisTry/show_events1.py
@@ -0,0 +1,165 @@
+from re import U
+import mysql.connector
+
+# import sys
+# sys.path.insert(0, "../src")
+# import login
+
+
+def connect():
+    dsn = {
+        "user": "maria",
+        "password": "password",
+        "host": "127.0.0.1",
+        "port": "3306",
+        "database": "goldapp",
+        "raise_on_warnings": True,
+    }
+    try:
+        conn = mysql.connector.connect(**dsn)
+        cursor = conn.cursor(prepared=True)
+        return conn, cursor
+    except Exception as err:
+        print(err)
+
+def user_location():
+    """User location based on the email id they are signed in with."""
+    try:
+        conn, cursor = connect()
+        sql = """
+            SELECT
+                goldapp.location.place
+            FROM
+	            goldapp.user,
+	            goldapp.location
+            WHERE
+	            location.location_id = user.location_location_id
+                and email = ?
+            ;
+            """
+            # fetching logged in user from the text file
+        with open("src/current_email.txt", "r") as current_email:
+            email = current_email.readline()
+
+        args = (email,)
+        cursor.execute(sql, args)
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        print(result[0])
+        return result[0]            # The location
+
+    except Exception as err:
+        print(err)
+
+def events_details(events_id):
+    """This function will fetch all events from the database
+    based on the id's received.
+    """
+    id_tuple = tuple(events_id)
+    try:
+        conn, cursor = connect()
+        sql = f"""
+        SELECT
+            event.name,
+            event.eventdate,
+            event.eventtime,
+            event.address_line,
+            event.city,
+            interest.interest,
+            interest.image_path,
+            event.information
+        FROM event, interest
+        WHERE event.interest_interest_id = interest.interest_id
+        AND event.event_id IN {id_tuple}
+        ;
+        """
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        # print(result)
+        return result
+
+    except Exception as err:
+        print(err)
+
+
+def event_ids_list():
+    """This function will get the id's of the events that need to be displayed."""
+    try:
+        conn, cursor = connect()
+        sql = """
+            SELECT event_id
+            FROM event
+            JOIN user
+            ON user.location_location_id = event.location_id
+            JOIN user_has_interest
+            ON user.email = user_has_interest.user_email
+            AND event.interest_interest_id = user_has_interest.interest_interest_id
+            WHERE user.email = ?
+            ;
+            """
+        # fetching logged in user from the text file
+        with open("src/current_email.txt", "r") as current_email:
+            email = current_email.readline()
+
+        args = (email,)
+        cursor.execute(sql, args)
+        event_id = cursor.fetchall()
+        events = []
+        for i in event_id:
+            events.append(i[0])
+        # print(events)
+        cursor.close()
+        conn.close()
+        return events
+
+    except Exception as err:
+        print(err)
+
+
+def show_events():
+    """Call this function to get all the events in list of tuples."""
+    event_list = event_ids_list()
+    return event_list, events_details(event_list)
+
+
+def one_event(e_id):
+    """This function will fetch all events from the database
+    based on the id's received.
+    """
+    try:
+        conn, cursor = connect()
+        sql = f"""
+        SELECT
+            event.name,
+            event.eventdate,
+            event.eventtime,
+            event.address_line,
+            event.city,
+            interest.interest,
+            interest.image_path,
+            event.information
+        FROM event, interest
+        WHERE event.interest_interest_id = interest.interest_id
+        AND event.event_id = ?
+        ;
+        """
+        args = (e_id,)
+        cursor.execute(sql, args)
+        # cursor.execute(sql)
+        ename, edate, etime, eaddress, ecity, interest, image_path, event_desc = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        print(ename)
+        print(edate)
+        return ename, edate, etime, eaddress, ecity, interest, image_path, event_desc
+    
+    except Exception as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    print(show_events())
+    one_event(1)
